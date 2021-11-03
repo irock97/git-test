cd70873b15e478169b80523d193d5567df495986
irock97 <93647510+irock97@users.noreply.github.com>
2021-11-03 21:26:22 +0800

    pull commit

--- src/main/java/com/jj/git/GitTest.java
+++ src/main/java/com/jj/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("master test2");
         System.out.println("hot-fix test2");
         System.out.println("push test");
+        System.out.println("pull test");
+        
     }
 }
